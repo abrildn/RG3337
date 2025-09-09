a566a2dcd4b675f84fb95624759d162f3d274340
Abril <abrudomingueznoya@gmail.com>
2025-09-09 13:55:48 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar monto de compra:");
+            string linea = Console.ReadLine();
+            int monto = int.Parse(linea);
+
+            if (monto > 500000)
+            {
+                int RG3337 = monto * 5 / 10;
+                Console.WriteLine($"El monto a retener es: " + RG3337);
+            }
+            else
+            {
+                Console.WriteLine("No sos plausible de retención");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
